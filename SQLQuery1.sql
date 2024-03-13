10c0f9f7cd094e62e67f6a76073171ffb82c5ee2
Lucasvgb <lucasvgb@id.uff.br>
2024-03-13 18:30:38 -0300

    Update SQLQuery1.sql
    Função NULLIF e COALESCE

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,16 @@ GO
 SELECT Description, DiscountPct, MaxQty ,
 				ISNULL(MaxQty, 0) AS 'Max Quantity'
 FROM Sales.SpecialOffer
+
+--Usando o COALESCE: Ele aceita mais de dois parâmetros
+SELECT Description, DiscountPct, MaxQty , MinQty ,
+				COALESCE (MaxQty,MinQty, 0 ) AS 'Quantity'
+FROM Sales.SpecialOffer 
+
+--NULLIF: Retornar NULL se ambos os parâmetros forem nulos
+SELECT ProductID, MakeFlag, FinishedGoodsFlag, 
+					NULLIF(MakeFlag, FinishedGoodsFlag) AS 'Null if Equal'
+FROM Production.Product
+WHERE ProductID < 10
+
+--Se MakeFlag for é igual as FinishedGoodsFlag, a consulta vai retornar NULL
