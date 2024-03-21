7608e064c743e62cc97b01eaa5cbc9fe4d2f3848
Lucasvgb <lucasvgb@id.uff.br>
2024-03-21 11:44:48 -0300

    Create DistinctAS_ORDERBY.sql
    Distintic with order by

---
+++ DistinctAS_ORDERBY.sql
@@ -0,0 +1,12 @@
+/*
+Treinando usar o AS em algumas colunas, para facilitar o manuseio
+em conjunto, estou usando o DISTINTC antes do SELECT, e também 
+fazendo o uso do ORDER BY
+*/
+SELECT  USR_NOME AS 'Nome', USR_EMAIL AS 'Email', USR_COD AS 'Codigo'
+FROM ZUR010
+ORDER BY Nome, Email
+
+
+
+
